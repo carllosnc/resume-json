63727eb0d5eac3b7e844b3e21037d182fc8aade8
Carlos Costa <carllos.nc@gmail.com>
2025-01-17 14:19:05 -0300

    Add Skills component to display skills section in resume

---
+++ src/components/skills.tsx
@@ -0,0 +1,30 @@
+import { View, Text } from "@react-pdf/renderer"
+import { styles } from "@/style"
+import type { Resume } from "@/resume"
+
+export function Skills({ content }: { content: Resume }) {
+  return (
+    <View style={styles.section}>
+      <Text style={styles.section_title}>
+        {content.skills.title}
+      </Text>
+
+      <Text style={styles.text}>
+        {content.skills.description}
+      </Text>
+
+      {
+        content.skills.skill_section.map((item, index) => {
+          return (
+            <View key={`${index}`} style={styles.section_sub}>
+              <Text style={styles.text}>
+                <Text style={styles.feature}>{item.title} </Text>
+                {item.items}
+              </Text>
+            </View>
+          )
+        })
+      }
+    </View>
+  )
+}
