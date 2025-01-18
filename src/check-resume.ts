7a2dac1bb4d86e52f92a45f08223bc0f694b0cde
Carlos Costa <carllos.nc@gmail.com>
2025-01-18 03:02:25 -0300

    Enhance resume validation by adding required fields for personal info and work experiences

--- src/check-resume.ts
+++ src/check-resume.ts
@@ -2,11 +2,21 @@ import type { Resume } from "@/resume-type";
 
 export function checkContent(content: Resume) {
   const requiredFields = [
-    { field: content.name, message: "Name is required" },
-    { field: content.header, message: "Header is required" },
-    { field: content.skills, message: "Skills is required" },
-    { field: content.skills?.description, message: "Skills.description is required" },
-    { field: content.skills?.skill_section, message: "Skills.skill_section is required" },
+    { field: content.name, message: "{name} is required" },
+    { field: content.header, message: "{header} is required" },
+
+    { field: content.personal_info, message: "{personal_info} is required" },
+    { field: content.personal_info?.title, message: "{personal_info.title} is required" },
+    { field: content.personal_info?.info, message: "{personal_info.info} is required" },
+
+    { field: content.skills, message: "{skills} is required" },
+    { field: content.skills?.title, message: "{skills.title} is required" },
+    { field: content.skills?.description, message: "{skills.description} is required" },
+    { field: content.skills?.skill_section, message: "{skills.skill_section} is required" },
+
+    { field: content.work_experiences, message: "{work_experiences} is required" },
+    { field: content.work_experiences?.title, message: "{work_experiences.title} is required" },
+    { field: content.work_experiences?.experiences, message: "{work_experiences.experiences} is required" },
   ];
 
   for (const { field, message } of requiredFields) {
