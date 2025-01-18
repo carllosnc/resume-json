07a7da3e6388194f8056bbc2b466a450d84c1931
Carlos Costa <carllos.nc@gmail.com>
2025-01-18 17:28:11 -0300

    Refactor Extras component to use local styles for improved layout of extra items

--- src/components/extras.tsx
+++ src/components/extras.tsx
@@ -1,6 +1,14 @@
-import { View, Text } from "@react-pdf/renderer";
+import { View, Text, StyleSheet } from "@react-pdf/renderer";
 import { styles } from "@/style";
 import type { Resume } from "@/resume-type";
+
+const localStyles = StyleSheet.create({
+  items: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2px',
+  },
+})
 
 export function Extras({ content }: {content: Resume}){
   return (
@@ -9,15 +17,17 @@ export function Extras({ content }: {content: Resume}){
         { content.extras.title }
       </Text>
 
-      {
-        content.extras.items.map((item, index) => {
-          return (
-            <Text key={`${index}`} style={styles.text}>
-              • { item }
-            </Text>
-          )
-        })
-      }
+      <View style={localStyles.items}>
+        {
+          content.extras.items.map((item, index) => {
+            return (
+              <Text key={`${index}`} style={styles.text}>
+                • { item }
+              </Text>
+            )
+          })
+        }
+      </View>
     </View>
   )
 }
